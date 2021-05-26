9ac0b0ce681daa5a6ea4c5836da11489db6b37d4
NikosBakalis <bakalis.nikolas@gmail.com>
2021-05-26 22:10:43 +0300

    Probability added.

--- DaMemer.js
+++ DaMemer.js
@@ -1,6 +1,9 @@
-const Discord = require('discord.js');
+FILE_LOCATION_EGW = 'word_texts/Egw.txt';
 
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const lineReader = require('line-reader');
+
+const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('DaMemer is up online!');
@@ -9,8 +12,23 @@ client.once('ready', () => {
 client.login('ODQ3MTE1NTg4MjM3MDAwNzI2.YK5X3g.iSZtuOCxiSBaCJIMF6GqAKkJ2us');
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-      msg.channel.send('pong');
+    if(probability(1)) {
+        msg.reply('ΜΑΛΑΚΑ ΒΟΥΛΩΝΕ 👌');
     }
+    if(msg.content === 'ping' && probability(100)) {
+      msg.reply('pong');
+    }
+    lineReader.eachLine(FILE_LOCATION_EGW, function(line) {
+        if(msg.content.includes(line) && probability(20)) {
+            msg.reply('ΚΑΛΑ ΕΣΥ!');
+        }
+    });
 });
+
+function probability(x) {
+    num = Math.floor(Math.random() * 100) + 1;
+    if(num <= x) {
+        return true;
+    }
+    else return false;
+}
